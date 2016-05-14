40f00e55df566c6e66dc24b496b2c4536a0f6d86
arnaimbd <arnaimbd@gmail.com>
2016-05-14 19:11:27 +0600

    wc payment complete hoook init

--- wc-stock-availabilty.php
+++ wc-stock-availabilty.php
@@ -14,11 +14,23 @@ function wc_stock_availability_placement(){
 	$total_units = number_format($product->stock,0,'','');
 	$units_sold = get_post_meta( $product->id, 'total_sales', true );
 	echo '<p>' . sprintf( __( 'Sold %s out of %s', 'woocommerce' ), $units_sold, $total_units ) . '</p>';
+
+	global $wpdb;
+	$product_id = 37;
+
+	// get_author_meta();
+    $author_id = $wpdb->get_var("SELECT post_author FROM $wpdb->posts WHERE $wpdb->posts.ID = $product_id");
+
+    $author_email = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE user_id = $author_id AND meta_key = 'billing_email'");
+
+    echo $author_email;
 }
 
 add_action( 'woocommerce_after_shop_loop_item', 'wc_stock_availability_placement', 6);	
 add_action( 'woocommerce_before_add_to_cart_form', 'wc_stock_availability_placement', 10, 0);
 
+
+// 
 function hide_default_stock_message(){
 	echo '<style> .in-stock{display:none;} </style>';
 }
@@ -26,13 +38,14 @@ function hide_default_stock_message(){
 add_action( 'wp_head', 'hide_default_stock_message', 11, 0 );
 
 
-
+// 
 function wc_stock_availability_admin_menu() {
 	add_menu_page('WC Stock Availability', 'WC Stock', 'administrator', 'wc_stock_availability_admin_settings', 'wc_stock_availability_settings_page', 'dashicons-admin-generic');
 }
 add_action('admin_menu', 'wc_stock_availability_admin_menu');
 
 
+// 
 function wc_stock_availability_admin_settings() {
 	register_setting( 'wc_stock_availability_settings_group', 'wc_sa_name' );
 	register_setting( 'wc_stock_availability_settings_group', 'wc_sa_phone' );
@@ -40,6 +53,7 @@ function wc_stock_availability_admin_settings() {
 }
 add_action( 'admin_init', 'wc_stock_availability_admin_settings' );
 
+// 
 function wc_stock_availability_settings_page(){ ?>
 	<div class="wrap">
 	<h2>Settings</h2>
@@ -71,6 +85,7 @@ function wc_stock_availability_settings_page(){ ?>
 <?php }
 
 
+// 
 function shop_inventory() {
 
   global $product;
@@ -78,20 +93,73 @@ function shop_inventory() {
   $total_units = number_format($product->stock,0,'','');
   $units_sold = get_post_meta( $product->id, 'total_sales', true );
 
-  	$order = new WC_Order(105);
-	$items = $order->get_items();
-	foreach ( $items as $item ) {
-	  echo $product_id = $item['product_id'] . '<br/>';
-	  echo $product_name = $item['name'] . '<br>' ;
-	  echo $product_variation_id = $item['variation_id'] . '<br>';
-	}
+ //  	$order = new WC_Order(105);
+	// $items = $order->get_items();
+	// foreach ( $items as $item ) {
+	//   echo $product_id = $item['product_id'] . '<br/>';
+	//   echo $product_name = $item['name'] . '<br>' ;
+	//   echo $product_variation_id = $item['variation_id'] . '<br>';
+	// }
 
-  // if ( $units_sold >= $total_units ) {
-  //   echo 'send email';
-  // } else{
-  // 	echo 'don\'t execute mail';
-  // }
+  if ( $units_sold >= $total_units ) {
+    echo 'send email';
+  } else{
+  	echo 'don\'t execute mail';
+  }
+  // 
+ //  $args = array(
+	//   'post_type' => 'shop_order',
+	//   'post_status' => 'publish',
+	//   'meta_key' => '_customer_user',
+	//   'posts_per_page' => '-1'
+	// );
+	// $my_query = new WP_Query($args);
 
+	// $customer_orders = $my_query->posts;
+
+	// foreach ($customer_orders as $customer_order) {
+	//  $order = new WC_Order();
+
+	//  $order->populate($customer_order);
+	//  $orderdata = (array) $order;
+	//  echo '<pre>';
+	//  print_r( $orderdata );
+	 
+	//  echo '<pre/>';
+
+	// }
 }
 
-add_action( 'woocommerce_after_shop_loop_item', 'shop_inventory', 8);
+function is_preorder_exceded( $product_id ){
+	$total_units = get_post_meta( $product_id, '_stock', true );
+	// $units_sold  = get_post_meta( $product_id, 'total_sales', true );
+	$preorder_unit = 2;
+
+	return $total_units < $preorder_unit;
+}
+
+function action_woocommerce_payment_complete( $order_id ) { 
+
+    $order = new WC_Order( $order_id );
+	$items = $order->get_items();
+
+	foreach ( $items as $item ) {
+	    $product_qty = $item['qty'];
+	    // ${ 'sold_qty_of_' . $product_id } = $item['qty'] . '<br/><br/>';
+	    // $product_name = $item['name'] . '<br>';
+	    $product_id = $item['product_id'] . '<br>';
+	    if ( is_preorder_exceded( $product_id ) ) {
+
+	    }
+	}
+
+}     
+
+add_action( 'woocommerce_payment_complete', 'action_woocommerce_payment_complete', 10, 1 ); 
+
+
+
+function custom_thankyou_message( $this_id ) {
+	do_action( 'woocommerce_payment_complete',  $this_id ); 
+}
+add_action ('woocommerce_thankyou', 'custom_thankyou_message');
